9543b11330565fd31dff6e25271b90dd0ed7549a
Gosh <osvaldocube@gmail.com>
2015-01-17 11:19:44 -0600

    Initial Commit

---
+++ Testing3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testing3
+//
+//  Created by TRON on 17/01/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Testing3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing3
+//
+//  Created by TRON on 17/01/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
